88b94774977279b74059dc32623cca2c7bc2fbf4
Ben Foster <benfoster4130@gmail.com>
2024-10-16 10:07:03 +1100

    render perf

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import * as ExifReader from "exifreader";
 import { FormEvent, useEffect, useRef, useState } from "react";
-// import "./App.css";
 
-// async function getFile() {
-//   // Open file picker and destructure the result the first handle
-//   const [fileHandle] = await window.showOpenFilePicker({
-//     types: [
-//       {
-//         description: "Images",
-//         accept: {
-//           "image/*": [".jpeg", ".jpg"],
-//         },
-//       },
-//     ],
-//     excludeAcceptAllOption: true,
-//     multiple: false,
-//   });
-//   const file = await fileHandle.getFile();
-//   return file;
-// }
+interface PhotoData {
+  url: string;
+  file: File;
+  exif: ExifReader.Tags | null;
+  image: HTMLCanvasElement;
+  width: number;
+  height: number;
+}
+
+const CANVAS_WIDTH = 1440;
+const CANVAS_HEIGHT = 1440;
+const CANVAS_MARGIN = 25;
 
 function downloadImage(data: string, filename = "untitled.jpeg") {
   var a = document.createElement("a");
@@ -27,47 +21,6 @@ function downloadImage(data: string, filename = "untitled.jpeg") {
   document.body.appendChild(a);
   a.click();
 }
-
-// function squircleRect(x: number, y: number, w: number, h: number, n: number) {
-//   const squircleoid = new Path2D();
-//   if (typeof n === "undefined") n = 1.0;
-//   const hw = 0.5 * w;
-//   const hh = 0.5 * h;
-//   squircleoid.moveTo(x + hw, y); // top mid
-//   squircleoid.bezierCurveTo(
-//     x + hw * (1 - n),
-//     y,
-//     x,
-//     y + hh * (1 - n),
-//     x,
-//     y + hh
-//   ); // left mid
-//   squircleoid.bezierCurveTo(
-//     x,
-//     y + hh + n * hh,
-//     x + hw - n * hw,
-//     y + h,
-//     x + hw,
-//     y + h
-//   ); // bottom mid
-//   squircleoid.bezierCurveTo(
-//     x + hw + n * hw,
-//     y + h,
-//     x + w,
-//     y + hh + n * hh,
-//     x + w,
-//     y + hh
-//   ); // right mid
-//   squircleoid.bezierCurveTo(
-//     x + w,
-//     y + hh - n * hh,
-//     x + hw + n * hw,
-//     y,
-//     x + hw,
-//     y
-//   ); // top mid
-//   return squircleoid;
-// }
 
 function roundedRect(x: number, y: number, w: number, h: number, r: number) {
   const roundRect = new Path2D();
@@ -80,22 +33,55 @@ function roundedRect(x: number, y: number, w: number, h: number, r: number) {
   return roundRect;
 }
 
-function readFile(file: File): Promise<string> {
+function readFile(file: File): Promise<PhotoData> {
   return new Promise((resolve) => {
     if (file) {
       var reader = new FileReader();
 
-      reader.onloadend = function () {
-        resolve(reader.result as string);
-      };
+      reader.onloadend = async function () {
+        const tags = await ExifReader.load(file);
 
+        const img = new Image();
+        img.onload = () => {
+          const offscreenCanvas = document.createElement("canvas");
+
+          const marginL = CANVAS_MARGIN;
+          const marginR = CANVAS_MARGIN;
+          const marginT = CANVAS_MARGIN;
+          const marginB = CANVAS_MARGIN;
+
+          const aspect = img.naturalWidth / img.naturalHeight;
+
+          // calculate width and height if landscape
+          let w = CANVAS_WIDTH - marginL - marginR;
+          let h = w / aspect;
+
+          // size if portrait
+          if (h > CANVAS_HEIGHT - marginT - marginB) {
+            h = CANVAS_HEIGHT - marginT - marginB;
+            w = h * aspect;
+          }
+
+          offscreenCanvas.width = w;
+          offscreenCanvas.height = h;
+
+          offscreenCanvas.getContext("2d")?.drawImage(img, 0, 0, w, h);
+
+          resolve({
+            url: reader.result as string,
+            file,
+            exif: tags,
+            image: offscreenCanvas,
+            width: w,
+            height: h,
+          });
+        };
+        img.src = reader.result as string;
+      };
       reader.readAsDataURL(file);
     }
   });
 }
-
-const CANVAS_WIDTH = 1440;
-const CANVAS_HEIGHT = 1440;
 
 function App() {
   const canvas = useRef<HTMLCanvasElement | null>(null);
@@ -104,14 +90,8 @@ function App() {
   const [glow] = useState(false);
   const [bg, setBg] = useState<string>("black");
   const [rounded, setRounded] = useState<boolean>(true);
-  // const [file, setFile] = useState<File | null>(null);
   const [selectedFile, setSelectedFile] = useState<number>(0);
-  const [files, setFiles] = useState<Array<{
-    url: string;
-    file: File;
-    exif: ExifReader.Tags | null;
-  }> | null>(null);
-  // const [exif, setExif] = useState<ExifReader.Tags | null>(null);
+  const [files, setFiles] = useState<Array<PhotoData> | null>(null);
 
   const hasSelectedFile = files?.[selectedFile];
 
@@ -119,78 +99,77 @@ function App() {
     const ctx = canvas.current?.getContext("2d");
 
     if (ctx && files?.[selectedFile]) {
-      // const file = files[selectedFile].file;
       const exif = files[selectedFile].exif;
-      const img = new Image();
-      img.onload = () => {
-        const exposure = exif?.["ExposureTime"]?.description;
-        const ap = exif?.["FNumber"]?.description;
-        const iso = exif?.["ISOSpeedRatings"]?.description;
-        const focal = exif?.["FocalLengthIn35mmFilm"]?.description;
+      // const img = new Image();
+      // img.onload = () => {
+      const exposure = exif?.["ExposureTime"]?.description;
+      const ap = exif?.["FNumber"]?.description;
+      const iso = exif?.["ISOSpeedRatings"]?.description;
+      const focal = exif?.["FocalLengthIn35mmFilm"]?.description;
 
-        const meta = [
-          focal ? focal + "mm" : false,
-          ap,
-          iso ? "ISO" + iso : false,
-          exposure ? exposure + "s" : false,
-        ]
-          .filter(Boolean)
-          .join(" | ");
+      const meta = [
+        focal ? focal + "mm" : false,
+        ap,
+        iso ? "ISO" + iso : false,
+        exposure ? exposure + "s" : false,
+      ]
+        .filter(Boolean)
+        .join(" | ");
 
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        ctx.fillStyle = bg;
-        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.fillStyle = bg;
+      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-        const marginL = 25;
-        const marginR = 25;
-        const marginT = 25;
-        const marginB = exifEnabled ? 100 : 25;
-        const radius = 30;
+      const marginL = CANVAS_MARGIN;
+      const marginR = CANVAS_MARGIN;
+      const marginT = CANVAS_MARGIN;
+      const marginB = exifEnabled ? 100 : CANVAS_MARGIN;
+      const radius = 30;
 
-        const aspect = img.naturalWidth / img.naturalHeight;
+      const aspect = files[selectedFile].width / files[selectedFile].height;
 
-        // calculate width and height if landscape
-        let w = CANVAS_WIDTH - marginL - marginR;
-        let h = w / aspect;
+      // calculate width and height if landscape
+      let w = CANVAS_WIDTH - marginL - marginR;
+      let h = w / aspect;
 
-        // size if portrait
-        if (h > CANVAS_HEIGHT - marginT - marginB) {
-          h = CANVAS_HEIGHT - marginT - marginB;
-          w = h * aspect;
-        }
+      // size if portrait
+      if (h > CANVAS_HEIGHT - marginT - marginB) {
+        h = CANVAS_HEIGHT - marginT - marginB;
+        w = h * aspect;
+      }
 
-        let x = marginL;
-        let y = ctx.canvas.height * 0.5 - h * 0.5;
+      let x = marginL;
+      let y = ctx.canvas.height * 0.5 - h * 0.5;
 
-        // position if portrait
-        if (h === CANVAS_HEIGHT - marginT - marginB) {
-          x = ctx.canvas.width * 0.5 - w * 0.5;
-          y = marginT;
-        }
-        if (glow) {
-          ctx.filter = "blur(300px)";
-          ctx.globalAlpha = 0.65;
-          ctx.drawImage(img, x, y, w, h);
-          ctx.filter = "none";
-          ctx.globalAlpha = 1;
-        }
+      // position if portrait
+      if (h === CANVAS_HEIGHT - marginT - marginB) {
+        x = ctx.canvas.width * 0.5 - w * 0.5;
+        y = marginT;
+      }
+      if (glow) {
+        ctx.filter = "blur(300px)";
+        ctx.globalAlpha = 0.65;
+        ctx.drawImage(files[selectedFile].image, x, y, w, h);
+        ctx.filter = "none";
+        ctx.globalAlpha = 1;
+      }
 
-        ctx.save();
-        if (rounded) {
-          ctx.clip(roundedRect(x, y, w, h, radius));
-        }
-        ctx.drawImage(img, x, y, w, h);
-        ctx.restore();
+      ctx.save();
+      if (rounded) {
+        ctx.clip(roundedRect(x, y, w, h, radius));
+      }
+      ctx.drawImage(files[selectedFile].image, x, y, w, h);
+      ctx.restore();
 
-        if (exifEnabled) {
-          ctx.fillStyle = bg === "black" ? "#999" : "#999";
-          ctx.font = "bold 42px Courier New";
-          ctx.textAlign = "center";
-          ctx.textBaseline = "middle";
-          ctx.fillText(meta, CANVAS_WIDTH / 2, CANVAS_HEIGHT - 48);
-        }
-      };
-      img.src = files[selectedFile].url;
+      if (exifEnabled) {
+        ctx.fillStyle = bg === "black" ? "#999" : "#999";
+        ctx.font = "bold 42px Courier New";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(meta, CANVAS_WIDTH / 2, CANVAS_HEIGHT - 48);
+      }
+      // };
+      // img.src = files[selectedFile].url;
     }
   }, [files, selectedFile, exifEnabled, bg, glow, rounded]);
 
@@ -211,21 +190,15 @@ function App() {
 
             if (!first) return;
 
-            const filedata: Array<{
-              url: string;
-              file: File;
-              exif: ExifReader.Tags | null;
-            }> = [];
+            const filedata: Array<PhotoData> = [];
 
             for (let i = 0; i < fs.length; i++) {
-              let url = await readFile(fs[i]);
-              filedata.push({ url, file: fs[i], exif: null });
+              let photo = await readFile(fs[i]);
+              filedata.push(photo);
             }
 
-            const tags = await ExifReader.load(first);
-            // console.log(tags);
-            // setExif(tags);
-            filedata[0].exif = tags;
+            // const tags = await ExifReader.load(first);
+            // filedata[0].exif = tags;
 
             setSelectedFile(0);
             setFiles(filedata);
@@ -234,20 +207,6 @@ function App() {
         <label className="filebutton" htmlFor="fileinput">
           Open
         </label>
-        {/* <button
-          onClick={async () => {
-            const f = await getFile();
-
-            const tags = await ExifReader.load(f);
-
-            console.log(tags);
-
-            setExif(tags);
-            setFile(f);
-          }}
-        >
-          Open
-        </button> */}
         <span className="spacer" />
 
         <label className="cb">
@@ -362,31 +321,9 @@ function App() {
           )}
         </label>
         <span className="spacer" />
-        {/* <label>
-          <input
-            type="checkbox"
-            checked={glow}
-            onChange={(e: FormEvent<HTMLInputElement>) =>
-              setGlow(e.currentTarget.checked)
-            }
-          />
-          <span>Aura</span>
-        </label>*/}
-        {/* <label>
-          <input
-            type="color"
-            value={bg}
-            onChange={(e: FormEvent<HTMLInputElement>) =>
-              setBg(e.currentTarget.value)
-            }
-          />
-          <span>Background</span>
-        </label> */}
         <button
           disabled={!hasSelectedFile}
           onClick={async () => {
-            // var canvas = document.querySelector('#my-canvas');
-
             var dataURL = canvas.current?.toDataURL("image/jpeg", 1.0);
 
             if (!dataURL) return;
@@ -395,37 +332,6 @@ function App() {
               dataURL,
               `${files?.[selectedFile]?.file.name?.replace(".", "_")}_mat.jpeg`
             );
-            // if (!file) return;
-
-            // const options: SaveFilePickerOptions = {
-            //   types: [
-            //     {
-            //       description: "Images",
-            //       accept: {
-            //         "image/jpeg": [".jpeg"],
-            //       },
-            //     },
-            //   ],
-            //   suggestedName: `${file.name}_bordered.jpeg`,
-            // };
-
-            // canvas.current?.toBlob(
-            //   async (blob) => {
-            //     if (!blob) {
-            //       alert("Save error");
-            //       return;
-            //     }
-            //     const imgFileHandle = await window.showSaveFilePicker(options);
-
-            //     console.log("Save File chosen");
-
-            //     const writable = await imgFileHandle.createWritable();
-            //     await writable.write(blob);
-            //     await writable.close();
-            //   },
-            //   "image/jpeg",
-            //   1
-            // );
           }}
         >
           Save
@@ -442,17 +348,17 @@ function App() {
             <img
               onDragStart={(e) => e.preventDefault()}
               onClick={async () => {
-                if (!files[i].exif) {
-                  const tags = await ExifReader.load(files[i].file);
-                  setFiles((prev) => {
-                    if (!prev) return null;
+                // if (!files[i].exif) {
+                //   const tags = await ExifReader.load(files[i].file);
+                //   setFiles((prev) => {
+                //     if (!prev) return null;
 
-                    const updated = [...prev];
-                    updated[i].exif = tags;
+                //     const updated = [...prev];
+                //     updated[i].exif = tags;
 
-                    return updated;
-                  });
-                }
+                //     return updated;
+                //   });
+                // }
 
                 setSelectedFile(i);
               }}
